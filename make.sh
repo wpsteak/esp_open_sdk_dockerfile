eb2d5ca2cc8cba988a937d5076369f96b978d1a3
Wang Pin Shih <wpsteak@gmail.com>
2016-07-20 02:24:40 +0800

    add parameters support

--- make.sh
+++ make.sh
@@ -3,12 +3,27 @@ set -e
 
 DOCKER_IMAGE=esp_dev
 ESP_HOME=/opt/Espressif
-
 ESP_WORK_DIR="$ESP_HOME/esp-open-rtos/src/$(basename $PWD)"
+ESP_PORT="/dev/tty.SLAB_USBtoUART"
 
 PARAMS="$PARAMS -it --rm"
 PARAMS="$PARAMS -v $PWD:$ESP_WORK_DIR"
 PARAMS="$PARAMS -w $ESP_WORK_DIR"
+# PARAMS="$PARAMS --device=/dev/tty.SLAB_USBtoUART:/dev/USB0:rwm"
 
-eval docker run $PARAMS $DOCKER_IMAGE make $@
+if [[ "$@" == "flash" ]]
+then
+  make flash
+else
 
+  if [[ "$@" == "debug" ]]
+  then
+    eval docker run $PARAMS $DOCKER_IMAGE make clean
+    eval docker run $PARAMS $DOCKER_IMAGE make
+    make flash
+    screen $ESP_PORT 115200
+  else
+    eval docker run $PARAMS $DOCKER_IMAGE make $@
+  fi
+
+fi
