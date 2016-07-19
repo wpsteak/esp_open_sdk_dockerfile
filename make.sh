8f568f8840d4a2411f0aa0effe34bb0caab52165
Wang Pin Shih <wpsteak@gmail.com>
2016-07-20 00:39:15 +0800

    add script

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+DOCKER_IMAGE=esp_dev
+ESP_HOME=/opt/Espressif
+
+ESP_WORK_DIR="$ESP_HOME/src/$(basename $PWD)"
+
+PARAMS="$PARAMS -it --rm"
+PARAMS="$PARAMS -v $PWD:$ESP_WORK_DIR"
+PARAMS="$PARAMS -w $ESP_WORK_DIR"
+
+eval docker run $PARAMS $DOCKER_IMAGE make $@
+
