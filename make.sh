22b52b4a8a867540fcd86a1b4880b6c2d432e720
Wang Pin Shih <wpsteak@gmail.com>
2016-07-31 14:17:43 +0800

    add ESP_PORT

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ DOCKER_IMAGE=esp_dev
 ESP_HOME=/opt/Espressif
 ESP_WORK_DIR="$ESP_HOME/esp-open-rtos/src/$(basename $PWD)"
 ESP_PORT="/dev/tty.SLAB_USBtoUART"
+# ESP_PORT="/dev/tty.wchusbserial14110"
 
 PARAMS="$PARAMS -it --rm"
 PARAMS="$PARAMS -v $PWD:$ESP_WORK_DIR"
@@ -13,14 +14,14 @@ PARAMS="$PARAMS -w $ESP_WORK_DIR"
 
 if [[ "$@" == "flash" ]]
 then
-  make flash
+  make flash ESPPORT=$ESP_PORT
 else
 
   if [[ "$@" == "debug" ]]
   then
     eval docker run $PARAMS $DOCKER_IMAGE make clean
     eval docker run $PARAMS $DOCKER_IMAGE make
-    make flash
+    make flash ESPPORT=$ESP_PORT
     screen $ESP_PORT 115200
   else
     eval docker run $PARAMS $DOCKER_IMAGE make $@
